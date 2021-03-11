5a900576e951e8bf38684da28c9dd3c6eb01de2e
Pine Wu <octref@gmail.com>
2021-03-11 18:55:07 +0800

    Fix assets

--- src/highlighter.ts
+++ src/highlighter.ts
@@ -1,6 +1,6 @@
 import * as shiki from 'shiki'
 
-shiki.setOnigasmWASM('/shiki/dist/onigasm.wasm')
+shiki.setOnigasmWASM('/shiki/onigasm.wasm')
 shiki.setCDN('/shiki/')
 
 let highlighter: shiki.Highlighter

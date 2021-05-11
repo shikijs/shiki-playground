d786fbe27ea267a4bbee301db6d1e9997961b570
Pine Wu <octref@gmail.com>
2021-05-12 00:07:03 +0800

    chore: fix build for multipage

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        play: resolve(__dirname, 'play.html')
+      }
+    }
+  }
 })
